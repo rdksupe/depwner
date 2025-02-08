1550e2498ea19612649bc10528c5c735539dee9e
Harshit Tomar <harshitt345@gmail.com>
2025-02-08 12:05:31 +0530

    Add placeholders for fucntions
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

---
+++ preload.js
@@ -0,0 +1,13 @@
+// import { contextBridge, ipcRenderer } from "electron/renderer";
+const { ipcRenderer } = require('electron/renderer')
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld("depwnerStatus", {
+    getThreats: () => ipcRenderer.invoke("getThreats"),
+    getStats: () => ipcRenderer.invoke("getStats")
+})
+
+contextBridge.exposeInMainWorld("depwnerPreferences", {
+    get: () => ipcRenderer.invoke("getSettings"),
+    set: (newSettings) => ipcRenderer.send("setSettings", newSettings),
+})
