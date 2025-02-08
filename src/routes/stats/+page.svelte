884a4bcf987508e5beba894f74233f6d8b264928
Harshit Tomar <harshitt345@gmail.com>
2025-02-08 17:39:24 +0530

    Add content to stats page
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,12 @@
+<div class="text-center">
+	<h1>Trend Analysis</h1>
+	<p>
+		We analysed an advanced dataset of malware samples with when they were first detected, the
+		source and their malware family. The dataset comprised of more than 95 Million malware samples.
+		Our detection system is also trained on the same database
+	</p>
+	<input type="" name="" id="" />
+</div>
+
+<style>
+</style>
