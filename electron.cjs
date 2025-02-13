6a2fc0b769c5331afb8f95cb744c1db72b914740
Harshit Tomar <harshitt345@gmail.com>
2025-02-13 21:39:06 +0530

    Disabled frame for window

--- electron.cjs
+++ electron.cjs
@@ -5,8 +5,11 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
+            // nodeIntegration: true,
+            // contextIsolation: false
         }
     })
 
