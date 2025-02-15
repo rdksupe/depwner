e6de81b0a7d9b991ad7e79e649e7f3869daa984e
Harshit Tomar <harshitt345@gmail.com>
2025-02-15 13:25:31 +0530

    Add threats page styles
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- src/routes/threats/+page.svelte
+++ src/routes/threats/+page.svelte
@@ -96,3 +96,44 @@
 		{/each}
 	</div>
 </div>
+
+<style>
+	h1 {
+		font-size: min(5vh, 5vw);
+		font-weight: 800;
+		margin-top: 2vh;
+		margin-bottom: -2vh;
+	}
+	.mainCont {
+		display: grid;
+		width: 80vw;
+		grid-template-columns: repeat(auto-fill, minmax(min(30vh, 30vw), 1fr));
+		gap: min(5vh, 5vw);
+		place-items: center;
+		padding: min(5vh, 5vw);
+	}
+	.entry {
+		aspect-ratio: 1;
+		display: grid;
+		place-items: center;
+		background: rgb(var(--ctp-base));
+		width: stretch;
+		border-radius: 2vh;
+		/* padding: min(5vh, 5vw); */
+
+		:global(svg) {
+			--size: min(15vh, 15vw);
+			height: var(--size);
+			width: var(--size);
+		}
+		h2 {
+			font-size: min(3.5vh, 3.5vw);
+			font-weight: 600;
+		}
+	}
+	.entry:hover {
+		color: rgb(var(--ctp-blue));
+		background: rgb(var(--ctp-surface0));
+		transition: all 0.7s cubic-bezier(0.19, 1, 0.22, 1);
+	}
+</style>
