0741d8ed6940ccc0b4d67b79a504c2149b3dfb01
Harshit Tomar <harshitt345@gmail.com>
2025-02-17 01:01:34 +0530

    Add styles to settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -116,3 +116,209 @@
 	</div>
 </div>
 
+<style>
+	.locationSettings {
+		display: grid;
+		place-items: center;
+		gap: 0.5vw;
+		margin-bottom: min(3vh, 3vw);
+		max-height: min(38vh);
+		overflow-y: auto;
+		overflow-x: hidden;
+		scrollbar-width: thin;
+		div {
+			display: flex;
+			align-content: center;
+			justify-content: center;
+			gap: 0.5vw;
+			margin: 0.5vh 0;
+		}
+		.addButton {
+			width: min(50vh, 40vw);
+		}
+		.entry {
+			width: min(75vh, 60vw);
+			div {
+				margin: 0;
+				border-radius: 1vh;
+				background: rgb(var(--ctp-crust));
+				padding: min(1vh, 1vw) min(1vw);
+				font-size: min(2vh, 1.9vw);
+				width: stretch;
+			}
+			button {
+				width: max-content;
+				color: rgb(var(--ctp-red));
+				background: rgb(var(--ctp-crust));
+			}
+			button:hover {
+				color: rgb(var(--ctp-crust));
+				background: rgb(var(--ctp-red));
+			}
+		}
+	}
+	/* .addPath { */
+	/* 	background: rgb(var(--ctp-crust)); */
+	/* 	border-radius: 1vh; */
+	/* 	padding: 0 min(1.5vh, 1.7vw); */
+	/* 	font-size: min(3vh, 3vw); */
+	/* 	font-weight: 700; */
+	/* 	border: 0.2vh solid rgb(var(--ctp-crust)); */
+	/* } */
+	.locationSettings button {
+		background: rgb(var(--ctp-mantle));
+		color: rgb(var(--ctp-blue));
+		border-radius: 1vh;
+		padding: min(1vh, 1vw) min(1vw);
+		font-size: min(2vh, 1.9vw);
+		font-weight: 500;
+		/* border: 0.2vh solid rgb(var(--ctp-crust)); */
+		width: stretch;
+		transition: all 0.7s cubic-bezier(0.19, 1, 0.22, 1);
+	}
+	.locationSettings button:hover {
+		background: rgb(var(--ctp-blue));
+		color: rgb(var(--ctp-mantle));
+	}
+	/* #locationInput { */
+	/* 	background: rgb(var(--ctp-crust)); */
+	/* 	border-radius: 1vh; */
+	/* 	padding: min(1vh, 1vw) min(1vw); */
+	/* 	font-size: min(2vh, 1.9vw); */
+	/* 	font-weight: 500; */
+	/* 	border: 0.2vh solid rgb(var(--ctp-crust)); */
+	/* 	width: stretch; */
+	/* } */
+	/* select { */
+	/* 	background: rgb(var(--ctp-crust)); */
+	/* 	border-radius: 1vh; */
+	/* 	padding: 0 min(1vw); */
+	/* 	font-size: min(2vh, 1.9vw); */
+	/* 	font-weight: 500; */
+	/* 	border: 0.2vh solid rgb(var(--ctp-crust)); */
+	/* } */
+	/* select:focus { */
+	/* 	border: 0.2vh solid rgb(var(--ctp-text)); */
+	/* } */
+	.headingInfo {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 1vw;
+		:global(svg) {
+			margin-top: min(0.5vh, 0.6vw);
+			--size: min(3vh, 3vw);
+			height: var(--size);
+			width: var(--size);
+			stroke: rgb(var(--ctp-overlay1));
+			transition: all 0.7s cubic-bezier(0.19, 1, 0.22, 1);
+		}
+		:global(svg):hover {
+			/* stroke: rgb(var(--ctp-text)); */
+			stroke: white;
+			opacity: 0.95;
+		}
+	}
+	.settingsField {
+		display: flex;
+		align-content: center;
+		justify-content: center;
+		/* max-width: 80vh; */
+		/* width: 60vw; */
+		gap: 1vw;
+		margin-bottom: min(3vh, 3vw);
+	}
+	h3 {
+		font-size: min(3vh, 2.5vw);
+	}
+	h2 {
+		font-size: min(4vh, 4vw);
+		font-weight: 800;
+	}
+	/* h1 { */
+	/* 	position: absolute; */
+	/* 	font-size: min(6vh, 5vw); */
+	/* 	font-weight: 200; */
+	/* 	top: 0; */
+	/* 	right: 0; */
+	/* 	left: 0; */
+	/* 	margin: 4vh auto; */
+	/* } */
+	.toggleButton {
+		--toggleWidth: min(7vh, 7vw);
+		--toggleHeight: min(3.5vh, 3.5vw);
+		display: grid;
+		place-items: center;
+	}
+
+	#checkboxInput {
+		display: none;
+	}
+	.toggleSwitch {
+		width: var(--toggleWidth);
+		height: var(--toggleHeight);
+		border-radius: 100vh;
+		transition-duration: 0.2s;
+	}
+
+	.toggleSwitch::after {
+		--toggleSizeFrac: 0.6;
+		--toggleMarginFrac: 0.2;
+		content: '';
+		position: absolute;
+		height: calc(var(--toggleSizeFrac) * var(--toggleHeight));
+		width: calc(var(--toggleSizeFrac) * var(--toggleHeight));
+		left: calc(var(--toggleMarginFrac) * var(--toggleHeight));
+		background-color: transparent;
+		border-radius: 100%;
+		transition-duration: 0.2s;
+		border: calc(0.15 * var(--toggleHeight)) solid white;
+	}
+
+	#checkboxInput:checked + .toggleSwitch::after {
+		/* transform: translateX(100%); */
+		left: calc(
+			var(--toggleWidth) - (var(--toggleSizeFrac) + var(--toggleMarginFrac)) * var(--toggleHeight)
+		);
+		transition-duration: 0.2s;
+		background-color: white;
+	}
+	/* Switch background change */
+	#checkboxInput:checked + .toggleSwitch {
+		background-color: rgb(var(--ctp-blue));
+		transition-duration: 0.2s;
+	}
+
+	#checkboxInputSch {
+		display: none;
+	}
+	#checkboxInputSch:checked + .toggleSwitch::after {
+		/* transform: translateX(100%); */
+		left: calc(
+			var(--toggleWidth) - (var(--toggleSizeFrac) + var(--toggleMarginFrac)) * var(--toggleHeight)
+		);
+		transition-duration: 0.2s;
+		background-color: white;
+	}
+	/* Switch background change */
+	#checkboxInputSch:checked + .toggleSwitch {
+		background-color: rgb(var(--ctp-blue));
+		transition-duration: 0.2s;
+	}
+
+	.mainCont {
+		background: rgb(var(--ctp-base));
+		height: 85vh;
+		width: stretch;
+		margin: 0 5vw;
+		max-width: 110vh;
+		border-radius: 1.5vh;
+		text-align: center;
+		display: grid;
+		align-items: center;
+		position: relative;
+	}
+	input {
+		color-scheme: dark;
+	}
+</style>
