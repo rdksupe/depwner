fcecaf4404558a1e3668ca9e3ac86fe60dfbf3ee
Harshit Tomar <harshitt345@gmail.com>
2025-02-16 16:53:26 +0530

    Add basic structure of scan page

--- src/routes/scan/+page.svelte
+++ src/routes/scan/+page.svelte
@@ -9,3 +9,31 @@
 	});
 
 </script>
+{#if status.status == 'scan'}
+	yo
+{:else}
+	<div class="mainCont grid text-center">
+		<button
+			class="scanButton row-start-1"
+		>
+			<div>
+				<h2>Full System Scan</h2>
+				<p>Scan all the files on the monitored filesystem paths</p>
+			</div>
+		</button>
+		<button
+			class="scanButton row-start-2"
+		>
+			<div>
+				<h2>Custom Scan</h2>
+				<p>Scan a particular File or Folder</p>
+			</div>
+		</button>
+		<div class="statusPannel row-span-2">
+			<!-- <button class="lastReportButton">Last Scan Report</button> -->
+			<h3>Active Hash Datasets</h3>
+			<h3 class="mt-[2vh]">Active Yara Datasets</h3>
+		</div>
+	</div>
+{/if}
+
