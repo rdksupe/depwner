230ad4b3839fa90eba41ac7c95e1cd8ac5e63557
Harshit Tomar <harshitt345@gmail.com>
2025-02-17 00:54:42 +0530

    Add content to logs page

--- src/routes/logs/+page.svelte
+++ src/routes/logs/+page.svelte
@@ -1,2 +1,33 @@
 <script>
+	let logs = $state([
+		{
+			scanType: 'Full',
+			filesScanned: 45372,
+			threats: 2,
+			time: 1737729873,
+			Folder: '/home/harshit/Harshit_Work/pclub/'
+		}
+	]);
+	let threats = $state(0);
+	let totalThreatsFound = $derived.by(() => {
+		let i = 0;
+		logs.forEach((log) => {
+			i += log.threats;
+		});
+		return i;
+	});
 </script>
+
+<div class="overallContainer">
+	<h2 class="overallThreats">Overall {totalThreatsFound} Threats were Eliminated by dePWNer</h2>
+	<h2 class="activeThreats">{threats} are Quarantined</h2>
+	<h2 class="eliminatedThreats">{totalThreatsFound - threats} were removed by user</h2>
+	<div class="table">
+		<h3>Time</h3>
+		<h3>Date</h3>
+		<h3>Scan Type</h3>
+		<h3>Path Scanned</h3>
+		<h3>Threats</h3>
+		<h3>TotalScanned</h3>
+	</div>
+</div>
