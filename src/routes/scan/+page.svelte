71530ea6fd9cb21a302b2940a41192b87c0bd4f8
Harshit Tomar <harshitt345@gmail.com>
2025-02-16 16:49:25 +0530

    Add status state to scan page

--- src/routes/scan/+page.svelte
+++ src/routes/scan/+page.svelte
@@ -2,4 +2,10 @@
 	const hashDatabases = ['loremIpsum', 'hydra', 'McAfee', 'Random', 'yoooooooo'];
 	const yaraDatabases = ['hydra', 'xylent', 'lauda', 'lassan', 'CHE bhai'];
 
+	let status = $state({
+		status: 'idle',
+		type: 'full',
+		progress: '10'
+	});
+
 </script>
