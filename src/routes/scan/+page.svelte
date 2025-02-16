436a8848862c7087f8eb6c6031fb68c91e2f66f4
Harshit Tomar <harshitt345@gmail.com>
2025-02-16 16:56:41 +0530

    Add styles to scan page

--- src/routes/scan/+page.svelte
+++ src/routes/scan/+page.svelte
@@ -61,3 +61,66 @@
 	</div>
 {/if}
 
+<style>
+	.entry {
+		margin: 0.5vh 0;
+	}
+	.mainCont {
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
+		gap: min(2vh, 2vw);
+	}
+	.scanButton {
+		background: rgb(var(--ctp-base));
+		border-radius: 1.5vh;
+		display: grid;
+		place-items: center;
+		h2 {
+			font-size: min(4vh, 2.5vw);
+			font-weight: 700;
+		}
+		div {
+			max-width: 30vw;
+		}
+		transition: all 0.7s cubic-bezier(0.19, 1, 0.22, 1);
+	}
+	.scanButton:hover {
+		background: rgba(var(--ctp-surface0));
+	}
+	.statusPannel {
+		border-radius: 1.5vh;
+		padding: min(7vh, 7vw) 0;
+		background: rgb(var(--ctp-base));
+		width: 35vw;
+		h3 {
+			font-size: min(4vh, 2.5vw);
+			font-weight: 700;
+		}
+		p {
+			font-size: min(2vh, 1.8vw);
+		}
+	}
+
+	.pinger {
+		display: block;
+		--size: min(1.2vh, 1.2vw);
+		height: var(--size);
+		width: var(--size);
+		background: oklch(0.897 0.196 126.665);
+		border-radius: 50%;
+		margin-left: 0.5vw;
+	}
+	.animate-ping-monitoring {
+		right: 0;
+		left: 0;
+		margin: auto;
+		animation: ping-monitor 2s cubic-bezier(0, 0, 0.2, 1) infinite;
+	}
+	@keyframes ping-monitor {
+		50%,
+		100% {
+			transform: scale(2.2);
+			opacity: 0;
+		}
+	}
+</style>
