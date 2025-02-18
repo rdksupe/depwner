1054f7143564dfd6f1847362e1395a8c83239919
Harshit Tomar <harshitt345@gmail.com>
2025-02-18 23:22:24 +0530

    Add info popup componenet

---
+++ src/components/+InfoPopup.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { info = $bindable() } = $props();
+</script>
+
+<button
+	class="absolute left-0 top-0 h-[100vh] w-[100vw] bg-black opacity-75"
+	aria-label="close popup"
+></button>
+
+<div class="mainCont">
+	<h2>{info.title}</h2>
+	<p>{info.message}</p>
+</div>
