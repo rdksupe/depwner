ec67bd1d5c2a4d15922305ce77aba33c7b6a42f4
Harshit Tomar <harshitt345@gmail.com>
2025-02-19 14:04:01 +0530

    Added content to InfoPopup

--- src/components/+InfoPopup.svelte
+++ src/components/+InfoPopup.svelte
@@ -5,6 +5,12 @@
 <button
 	class="absolute left-0 top-0 h-[100vh] w-[100vw] bg-black opacity-75"
 	aria-label="close popup"
+	onclick={() => {
+		info = {
+			title: '',
+			message: ''
+		};
+	}}
 ></button>
 
 <div class="mainCont">
